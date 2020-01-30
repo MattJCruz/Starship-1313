e66dc197528887fde2d99cdcc01c848242e7095b
MattJCruz <44383909+MattJCruz@users.noreply.github.com>
2020-01-30 14:15:12 -0600

    Experimenting with Starship 1313
    Combat changes, addition of fleshed-out turn-based battle system.
    Story Tweaks.
    Making structure more object-oriented.
    Implementing primitive save feature.

---
+++ s1313experiment.cpp
@@ -0,0 +1,26 @@
+//Take in current player health
+//Ask player what he will do?
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int choice;
+float mult;
+
+void combatLoop(int healthIn; )
+{
+    do
+    {
+        cout << "What will you do?";
+        cin >>
+    }
+    while()
+}
+class combatant()
+{
+    static int baseH = 15;
+    int atk1 = 3;
+}
+//Create combat function, use combatant class in combat function
